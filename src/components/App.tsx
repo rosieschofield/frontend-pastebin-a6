7453869c4a1f74fb7203614cd44d1ccebc1a7cb6
Rosie Schofield <rschofield00@gmail.com>
2023-08-29 12:53:09 +0100

    first get request with data rendered as a list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,32 @@
 import { MyComponent } from "./MyComponent";
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 
+interface Snippets {
+    id: number,
+    title: string,
+    code_snippet: string,
+    date: string
+}
+
 function App() {
+const [list, setList] = useState<Snippets[]>([])
+
+    async function fetchCodeSnippets() {
+        const res = await axios.get("https://pastebin-a6.onrender.com/")
+        const listOfSnippets = await res.data
+        setList(listOfSnippets)
+        console.log(list)
+    }
+    
+
+    fetchCodeSnippets()
+
     return (
         <div className="App">
             <MyComponent />
+            {list.map((elem)=> <li key={elem.id}>{elem.title}</li>)}
         </div>
     );
 }
